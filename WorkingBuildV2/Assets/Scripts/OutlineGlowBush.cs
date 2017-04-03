105319d6085adb94e656207b0009c39669e3f99c
DSU\crbarbera <crbarbera@pluto.dsu.edu>
2017-04-03 16:55:45 -0500

    Added shader to rabbit to show position behind things, started working on highlight for hiding spots

---
+++ WorkingBuildV2/Assets/Scripts/OutlineGlowBush.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineGlowBush : MonoBehaviour {
+
+    public Animation BushGlow;
+    public Animator anime;   
+    private Ray ray;
+    private RaycastHit hit;
+    private bool isPlaying = false;
+    private Shader baseShader;
+
+    void Start()
+    {
+        baseShader = GetComponent<Shader>();
+        anime = GetComponent<Animator>();
+        BushGlow = GetComponent<Animation>();
+    }
+
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //As long as the mouse hovers over object
+        if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("Tree"))
+        {
+            BushGlow.Play();
+            //Debug.Log("Over object");
+            isPlaying = true;
+            //anime.Play("OutlineGlowBush");           
+            anime.SetBool("IsPlaying", true);                 
+        }
+        else //As long as the mouse is not hovering over the object
+        {
+            BushGlow.Stop();
+            //Debug.Log("Not over object");
+            //anime.SetBool("IsPlaying", false);            
+        }
+
+    }
+}
