ca311bbb6aba718473ed35bacf6232ef23e44eed
DSU\jjplaetz <jesse.plaetz@trojans.dsu.edu>
2017-04-25 17:17:04 -0500

    Made screenshots transitionable with buttons

---
+++ WorkingBuildV2/Assets/ScreenShotNavigation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+
+public class ScreenShotNavigation : MonoBehaviour {
+
+    public Texture[] screenShots;
+    public RawImage rawImage;
+    public Button next;
+    public Button prev;
+
+    public int imageIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rawImage.texture = screenShots[imageIndex];
+         if(imageIndex >= 10) {
+            SceneManager.LoadScene("Title");
+        }
+	}
+
+    public void NextPage() {
+        imageIndex++;
+    }
+
+    public void PreviousPage() {
+        imageIndex--;
+
+        if (imageIndex < 0)
+            imageIndex = 0;
+    }
+}
